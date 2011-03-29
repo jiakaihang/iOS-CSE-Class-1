d15be4d61de536aab82af1f8e51ac3d1b97f4f21
Kaihang Hank Jia <khji462@chil08.orchestra.cse.unsw.edu.au>
2011-03-29 16:06:09 +1100

    person class added

---
+++ Classes/Person.m
@@ -0,0 +1,18 @@
+//
+//  Person.m
+//  Class1
+//
+//  Created by Kaihang Hank Jia on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Person.h"
+
+
+@implementation Person
+@interface Persons : NSObject
+{
+		
+}
+
+@end
